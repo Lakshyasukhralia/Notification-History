50319e67456e073f35f2e72c63561bb4be7e4be1
lakshya <lakshya@apna.co>
2024-09-10 16:31:12 +0530

    Added save history

--- app/src/main/java/com/sukhralia/notificationhistory/feature/tracker/presentation/history/HistoryScreen.kt
+++ app/src/main/java/com/sukhralia/notificationhistory/feature/tracker/presentation/history/HistoryScreen.kt
@@ -1,5 +1,6 @@
 package com.sukhralia.notificationhistory.feature.tracker.presentation.history
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,21 +9,29 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.sukhralia.notificationhistory.R
 import com.sukhralia.notificationhistory.util.formatDateTime
+import com.sukhralia.notificationhistory.util.saveFileToDownloads
 import java.util.Date
 
 @Composable
@@ -31,6 +40,8 @@ fun HistoryScreen(title: String, packageName: String) {
     val viewModel: HistoryViewModel = viewModel()
 
     val uiState by viewModel.uiState.collectAsState()
+
+    val context = LocalContext.current
 
     LaunchedEffect(key1 = Unit) {
         viewModel.getNotificationByTitle(title, packageName)
@@ -94,6 +105,38 @@ fun HistoryScreen(title: String, packageName: String) {
             }
         }
     }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.BottomEnd
+    ) {
+        FloatingActionButton(
+            onClick = {
+                val notificationStrings: List<String> = uiState.notifications.map { notification ->
+                    "${formatDateTime(Date(notification.createDate!!))} : ${notification.title}\n${notification.message}\n_____________________________\n"
+                }
+                val savedFile = saveFileToDownloads(
+                    context,
+                    "recovery_wiz_${title}_${packageName}_${Date()}",
+                    notificationStrings
+                )
+                Toast.makeText(context, "Saved in Downloads", Toast.LENGTH_LONG).show()
+            },
+            modifier = Modifier
+                .padding(bottom = 16.dp),
+            containerColor = Color.White
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_download),
+                modifier = Modifier.size(32.dp),
+                tint = Color.Black,
+                contentDescription = "Save"
+            )
+        }
+    }
+
 }
 
 
--- app/src/main/java/com/sukhralia/notificationhistory/util/Util.kt
+++ app/src/main/java/com/sukhralia/notificationhistory/util/Util.kt
@@ -1,15 +1,23 @@
 package com.sukhralia.notificationhistory.util
 
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import android.graphics.drawable.Drawable
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
 import android.provider.Settings
 import android.text.TextUtils
-import androidx.core.content.ContextCompat
+import android.util.Log
 import androidx.core.content.ContextCompat.startActivity
 import com.sukhralia.notificationhistory.feature.tracker.domain.model.AppInfo
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -51,6 +59,51 @@ fun getInstalledApps(context: Context): List<AppInfo> {
     return apps
 }
 
-fun getAppIcon(context: Context, icon: Int): Drawable? {
-    return ContextCompat.getDrawable(context, icon)
+fun saveTextFileToAppSpecificExternalStorage(
+    context: Context,
+    fileName: String,
+    stringList: List<String>
+): File? {
+    return try {
+        val file = File(context.getExternalFilesDir(null), fileName)
+
+        val fileOutputStream = FileOutputStream(file)
+        stringList.forEach { line ->
+            fileOutputStream.write((line + "\n").toByteArray())
+        }
+
+        fileOutputStream.close()
+        Log.d("File", "File saved successfully at: ${file.absolutePath}")
+        file
+    } catch (e: IOException) {
+        e.printStackTrace()
+        null
+    }
+}
+
+fun saveFileToDownloads(context: Context, fileName: String, content: List<String>) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
+            put(MediaStore.MediaColumns.MIME_TYPE, "text/plain")
+            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS)
+        }
+
+        val resolver = context.contentResolver
+        val uri = resolver.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, contentValues)
+
+        uri?.let {
+            try {
+                val outputStream: OutputStream? = resolver.openOutputStream(uri)
+                outputStream?.use { stream ->
+                    content.forEach { line ->
+                        stream.write((line + "\n").toByteArray())
+                    }
+                }
+                Log.d("File", "File saved successfully to Downloads")
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
 }
